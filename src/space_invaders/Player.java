e21af7cfeeb37137859b708ff2958ee791633158
Martin Hörnberg <martin@Martins-MacBook-Pro.local>
2021-02-25 22:06:29 +0100

    projekt

---
+++ src/space_invaders/Player.java
@@ -0,0 +1,11 @@
+package space_invaders;
+
+import javafx.scene.paint.Color;
+
+public class Player extends Sprite {
+	
+	public Player(int x, int y, int w, int h, String type, Color color) {
+		super(x,y,w,h,type,color);
+	}
+
+}
